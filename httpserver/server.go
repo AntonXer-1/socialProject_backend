b9928930b55e4cddd07a30c636cff5d3c0363d67
Evgeny Hodorev <hodorevevgenij6@gmail.com>
2026-04-16 21:42:45 +0300

    create http server

--- httpserver/server.go
+++ httpserver/server.go
@@ -1,1 +1,26 @@
 package httpserver
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type server struct {
+	handler *HTTPHandler
+}
+
+func CreateServer(handler *HTTPHandler) *server {
+	return &server{
+		handler: handler,
+	}
+}
+
+func (s *server) StartHTTPServer() {
+	router := mux.NewRouter()
+
+	router.Path("/user/registration").Methods("POST").HandlerFunc(s.handler.HandleRegistration)
+	router.Path("/user/authorization").Methods("GET").HandlerFunc(s.handler.HandleAuthorization)
+
+	http.ListenAndServe(":8080", router)
+}
--- main.go
+++ main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"os"
+	"socialProject_backend/httpserver"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	parents, parents_close := context.WithCancel(context.Background())
+	postgres, _ := context.WithCancel(parents)
+	defer parents_close()
+
+	fmt.Println("Starting server")
+
+	conn_url := os.Getenv("CONN_STRING")
+	conn, err := pgx.Connect(postgres, conn_url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error create conn: %v\n", err)
+		return
+	}
+
+	if er := conn.Ping(postgres); er != nil {
+		fmt.Fprintf(os.Stderr, "Error ping: %v\n", err)
+		return
+	}
+
+	handler := httpserver.CreateHTTPHandler(conn, parents)
+	server := httpserver.CreateServer(handler)
+
+	server.StartHTTPServer()
+}
